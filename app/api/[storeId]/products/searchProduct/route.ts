18169d41203e9bd8b5a6bb6f548af6aa8c5c2507
mhkormoh <moriehenryk@gmail.com>
2024-10-01 15:19:27 +0000

    Search Added

--- app/api/[storeId]/products/searchProduct/route.ts
+++ app/api/[storeId]/products/searchProduct/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
+import cors from "@/lib/cors";
 
 export async function GET(req: Request, res: Response) {
-  //await runMiddleware(req, res, cors); // Apply CORS middleware
+  await runMiddleware(req, res, cors); // Apply CORS middleware
 
   const { searchParams } = new URL(req.url);
   const query = searchParams.get("query") || "";  // Extract query parameter
@@ -25,7 +26,7 @@ export async function GET(req: Request, res: Response) {
     });
 
     const response = NextResponse.json(products);
-    response.headers.set('Access-Control-Allow-Origin', 'https://fenbaya-admin.vercel.app');
+    response.headers.set('Access-Control-Allow-Origin', 'https://fenbaya-client.vercel.app');
     return response;
   } catch (error) {
     console.error("[PRODUCT_SEARCH_ERROR]", error);
